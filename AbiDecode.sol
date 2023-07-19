527e4983f5c4b29da2c2c05bf787c13323364b46
Luke Sheppard <sheppiegit@gmail.com>
2023-07-19 12:03:53 -0400

    structr.

--- AbiDecode.sol
+++ AbiDecode.sol
@@ -4,4 +4,9 @@ pragma solidity 0.8.18;
 //abi.encode encodes data into bytes
 //abi.decode decodes bytes into data
 
-contract AbiDecode {}
+contract AbiDecode {
+    struct MyStruct {
+        string name;
+        uint256[2] nums;
+    }
+}
