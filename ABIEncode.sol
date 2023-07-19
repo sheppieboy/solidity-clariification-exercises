b3faf0f6026b9dbf880ec3401550f8f8707873d7
Luke Sheppard <sheppiegit@gmail.com>
2023-07-19 11:57:35 -0400

    added functions for encodeWithSelector and encodeWithSignature.

--- ABIEncode.sol
+++ ABIEncode.sol
@@ -8,3 +8,18 @@ interface IERC20 {
 contract Token {
     function transfer(address, uint256) external {}
 }
+
+contract AbiEncode {
+    function test(address _contract, bytes calldata data) external {
+        (bool ok,) = _contract.call(data);
+        require(ok, "call failed");
+    }
+
+    function encodeWithSignature(address to, uint256 amount) external pure returns (bytes memory) {
+        return abi.encodeWithSignature("transfer(address,uint256)", to, amount);
+    }
+
+    function encodeWithSelector(address to, uint256 amount) external pure returns (bytes memory) {
+        return abi.encodeWithSelector(IERC20.transfer.selector, to, amount);
+    }
+}
